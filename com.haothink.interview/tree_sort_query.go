a3b2b584516d592cb1ab4d6fcbafea5112ff1feb
wanghao.629 <wanghao.629@bytedance.com>
2023-05-05 15:16:44 +0800

    中序遍历

---
+++ com.haothink.interview/tree_sort_query.go
@@ -0,0 +1,42 @@
+package com_haothink_interview
+
+// 1 4 6 8
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type LinkedNode struct {
+	Val  int
+	Next *LinkedNode
+}
+
+var sortedNodeList = make([]*TreeNode, 0)
+
+func MiddleSortQuery(treeNode *TreeNode) *LinkedNode {
+
+	if treeNode == nil {
+		return &LinkedNode{}
+	}
+	sortNode(treeNode)
+	// sorted list
+	preHead := &LinkedNode{}
+	linkedPointer := preHead
+	for _, node := range sortedNodeList {
+		linkedPointer.Next = &LinkedNode{
+			Val: node.Val,
+		}
+		linkedPointer = linkedPointer.Next
+	}
+	return preHead.Next
+}
+
+func sortNode(treeNode *TreeNode) {
+
+	if treeNode != nil {
+		sortNode(treeNode.Left)
+		sortedNodeList = append(sortedNodeList, treeNode)
+		sortNode(treeNode.Right)
+	}
+}
